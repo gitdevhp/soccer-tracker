4d1f4a3d8bad927844f3e53c44811bc9da6f5943
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-07-06 01:15:55 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
+      <img src='/src/img/field.png' alt='Field'></img>
     </div>
   );
 }
