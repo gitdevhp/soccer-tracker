8699b885519904da7fabe611559fa009c3623041
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-07-07 17:24:57 +0000

    added dashboard logic, need settings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import Field from "./pages/Field"
 import Dashboard from "./pages/Dashboard"
+import Settings from "./pages/Settings"
  
   
 function App() {
   return (
     <Router>
       <Routes>
-          <Route exact path="/"  element={<Homepage/>} />
-          <Route path="/register"  element={<Field/>} />
-          <Route path="/dashboard"  element={<Dashboard/>} />
+          <Route exact path="/"  element={<Dashboard/>} />
+          <Route path="/field"  element={<Field/>} />
+          <Route path="/settings"  element={<Settings/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,42 @@
+import React, { useState, useRef } from "react"
+import Layout from "../components/Layout"
+
+function Dashboard() {
+    const [hasDataGot, setHasDataGot] = useState(false); //use to check if data has loaded
+    const gridCount = () => {
+        if (localStorage.getItem("gridReq") !== null) { //gridReq is a setting by the user to show amount of grids, not made yet
+            return JSON.parse(localStorage.getItem("gridReq"));
+        }
+    };
+    const makeGrids = () => {
+        const grids = { gridCount };
+        if (grids > 0) {
+            var indents = [];
+            for (var i = 0; i < grids; i++) {
+                indents.push(<div className='graph-${i}' key={i}></div>);
+            }
+            return indents;
+        } else {
+            return (
+                <div id="no-grids">
+                    <h1>No Grids Requested</h1>
+                    <p>You have not requested any grids yet, go to settings and customize</p>
+                </div>
+            )
+        }
+    };
+    return (
+        <>
+            <Layout>
+                <div id="data-flex">
+                    {makeGrids}
+                </div>
+                <div id="user-bar">
+                    <p id='name'>Hello, User!</p> <p id='edition'>0.0.1</p>
+                </div>
+            </Layout>
+        </>
+    );
+}
+
+export default Dashboard;
