0bf490fe2e2e8947e1e3952d7b283c7a841c3ec2
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-07-14 23:23:11 +0000

    bro wtf

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,42 +1,33 @@
-import React, { useState, useRef } from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/Layout"
 import { Link } from 'react-router-dom';
 
 function Dashboard() {
-    const [hasDataGot, setHasDataGot] = useState(false); //use to check if data has loaded
-    const snagGridSetting = () => {
-        if (localStorage.getItem("preset") !== null) { //gridReq is a setting by the user to show amount of grids, not made yet
-            return JSON.parse(localStorage.getItem("preset"));
-        }
-    };
-    const makeGrids = () => {
-        const grids = { snagGridSetting };
-        if (grids !== 'basePreset'){
-            return <h2>No Graph Set Yet, Visit Settings to Customize</h2>
-        }
-        else if (grids !== null) {
-            var indents = [];
-            for (var i = 0; i < grids.graphs.length; i++) {
-                indents.push(<div className='graph-${i} flexGraph' key={i}>
-                    <p>{grids.graphs[i].id}</p>
-                    <p>{grids.graphs[i].data}</p>
-                    <p>{grids.graphs[i].format}</p>
-                </div>);
-            }
-            return indents;
-        } else {
-            return (
-                <div id="no-grids">
-                    <h1>No Grids Requested</h1>
-                </div>
-            )
-        }
+    const [graphData, setGraphData] = useState([]);
+
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem('graphData'));
+        setGraphData(data);
+    }, []);
+
+    const makeGrids = ({ graphData }) => {
+        return (
+            <div>
+                {graphData.map((graph) => (
+                    <div key={graph.id}>
+                        <h3>Graph {graph.id + 1}</h3>
+                        <p>Data: {graph.data}</p>
+                        <p>Format: {graph.format}</p>
+                    </div>
+                ))}
+            </div>
+        );
     };
     return (
         <>
             <Layout>
                 <div id="data-flex">
-                    {makeGrids()}
+                    {makeGrids( graphData )}
                 </div>
                 <div id="user-bar">
                     <p id='name'>Hello, User!</p> <p className="text-center">Need to Adjust? <Link to="/settings">Settings</Link></p> <p id='edition'>0.0.1</p>
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,15 +1,19 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import GraphSetting from "../components/GraphSetting";
 import Layout from "../components/Layout";
 
-export var graphCount = 0;
-
 function LocalStorage() {
 
-    const [graphData, setGraphData] = useState([
-        { id: 0, data: "Tackles", format: "Bar" },
-    ]);
+    useEffect(() => {
+        if (localStorage.getItem('graphData')===null||localStorage.getItem('graphData')===undefined||localStorage.getItem('graphData')==='undefined'){
+             localStorage.setItem('graphData', JSON.stringify([{ id: graphCount, data: "Tackles", format: "Bar" }]));
+        } else{
+        loadGraphs();
+        }
+    }, []);
+
     const [graphCount, setGraphCount] = useState(1);
+    const [graphData, setGraphData] = useState([]);
 
     const addGraph = () => {
         setGraphCount(graphCount + 1);
@@ -18,16 +22,22 @@ function LocalStorage() {
     };
 
     const removeGraph = (id) => {
-        setGraphData(graphData.filter((graph) => graph.id !== id));
-        setGraphCount(graphCount - 1);
+        const newGraphData = graphData.filter((graph) => graph.id !== id);
+        setGraphData(newGraphData);
+        setGraphCount(newGraphCount => newGraphCount - 1);
+        // Update the id numbers of the remaining graphs
+        newGraphData.forEach((graph, index) => {
+            graph.id = index;
+        });
+        localStorage.setItem('graphData', JSON.stringify(newGraphData));
     };
 
     const updateGraph = (id, data, format) => {
         const updatedGraph = { id, data, format };
-        const index = graphData.findIndex((graph) => graph.id === id); //need help fixing, temp fix but whenever graph setting updated it creates new setting
+        const index = graphData.findIndex((graph) => graph.id === id);
         if (index === -1) {
             setGraphData([...graphData, updatedGraph]);
-        } else {    
+        } else {
             const newGraphData = [...graphData];
             newGraphData[index] = updatedGraph;
             setGraphData(newGraphData);
@@ -35,80 +45,35 @@ function LocalStorage() {
     };
 
     const saveGraphs = () => {
-        localStorage.setItem('graphData', JSON.stringify(graphData));
+        localStorage.setItem('graphPreset', JSON.stringify(graphData));
     };
 
     const loadGraphs = () => {
-        const data = JSON.parse(localStorage.getItem('graphData'));
+        const data = JSON.parse(localStorage.getItem('graphPreset'));
         if (data) {
             setGraphData(data);
             setGraphCount(data.length + 1);
         }
     };
 
-    /*let LocalSettings = {
-        "graphs": [
-            {
-                "id": {ide},
-                "data": {dataTemp},
-                "format": {format},
-            },
-        ]
-    }
-
-    const setGraph = (ID, data, format) => {
-        setide(ID);
-        setDataTemp(data); //or is it LocalSettings.graphID?
-        setFormatTemp(format);
-        localStorage.setItem('preset', JSON.stringify({ ...LocalSettings }));
-    }
-
-    const storeGraphs = () => {
-        for (let i = 0; i < document.querySelectorAll('GraphSet').length; i++) {
-            const formatt = document.querySelectorAll('GraphSet')[i].format;
-            const datt = document.querySelectorAll('GraphSet')[i].data;
-            setGraph(i,formatt,datt);
-        }
-    }
-
-    const addGraphSetting = () => {
-        graphCount++;
-        return (
-            <>
-                <GraphSetting data-id={graphCount}>
-
-                </GraphSetting>
-            </>
-        )
-    }*/
-
     return (
         <>
             <Layout>
                 <h3>Graph Settings</h3>
                 {graphData.map((graph) => (
-                    <GraphSetting 
-                    key={graph.id} 
-                    id={graph.id}
-                    data={graph.data}
-                    format={graph.format}
-                    updateGraph={updateGraph}
-                    removeGraph={removeGraph}
+                    <GraphSetting
+                        key={graph.id}
+                        id={graph.id}
+                        data={graph.data}
+                        format={graph.format}
+                        updateGraph={updateGraph}
+                        removeGraph={removeGraph}
                     />
-                 ))}
-                 {/*
-                 {[...Array(graphCount)].map((_, index) => (
-                 <GraphSetting
-                    key={index}
-                    id={index}
-                    updateGraph={updateGraph}
-                    removeGraph={removeGraph}
-                 />
-                 ))}
-                 */}
-                <button className="addGraph" onClick={ addGraph }>Add New Graph</button>
-                <button className="saveGraphs" onClick={ saveGraphs }>Save Graphs</button>
-                <button className="Load" onClick={ loadGraphs }>Load</button> {/*change 1st parameter to graph # must be fixed*/}
+                ))}
+                <button className="addGraph" onClick={addGraph}>Add New Graph</button>
+                <div className="nav">
+                    <p className="text-center" onClick={saveGraphs}><a href="/">Dashboard</a></p>
+                </div>
             </Layout>
         </>
     );
