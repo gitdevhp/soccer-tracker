d685fee1bd1f63e37cdb9a7cdc3087ff70252532
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-07-23 21:27:38 +0000

    some basi design optimizations to summary.js

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -8,7 +8,20 @@ function Summary() {
         <>
             <Layout>
                 <h3>Summary</h3>
-                <pre>{JSON.stringify(gameData, null, 2)}</pre>
+                <h3>Score: {gameData[1].score}</h3>
+                <div className="holdHalves">
+                    <div className="hdis">
+                        <h3>Half 1</h3>
+                        <h4>Time: {gameData[0].time}</h4>
+                        <h5>Passes: {gameData[0].Passes[0].Complete} - {gameData[0].Passes[0].Miss}</h5>
+                        
+                    </div>
+                    <div className="hdis">
+                        <h3>Half 2</h3>
+                        <h4>Time: {gameData[1].time}</h4>
+
+                    </div>
+                </div>
                 <Link to="/">Home</Link>
             </Layout>
         </>
