c80ac0063efe4dda986582ea5804c6537d129e0d
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-07-24 20:25:53 +0000

    poopoo

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import Layout from "../components/Layout"
 import { Link } from 'react-router-dom';
+import SettingsLogo from "../img/settings.png";
 
 function Dashboard() {
     const [graphData, setGraphData] = useState([]);
@@ -30,11 +31,15 @@ function Dashboard() {
     return (
         <>
             <Layout>
+                <h3>Dashboard</h3>
+                <div className="top-user-bar">
+                    <p id='name'>Hello, User!</p> <Link to="/settings"><img src={SettingsLogo} className="settingPNG" alt="Settings"/></Link>
+                </div>
                 <div id="data-flex">
                     {makeGrids()}
                 </div>
                 <div id="user-bar">
-                    <p id='name'>Hello, User!</p> <p className="text-center">Need to Adjust? <Link to="/settings">Settings</Link></p> <p><Link to="/field">Play</Link></p> <p id='edition'>0.0.1</p>
+                    <p><Link to="/field">Play</Link></p> <p id='edition'>0.0.1</p>
                 </div>
             </Layout>
         </>
